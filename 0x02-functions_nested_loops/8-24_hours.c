e5b9df6bce1f63e5c590c10bb2da9feea1209b7f
Khutso Mphiwe <mphiwekhutso@gmail.com>
2022-10-19 02:16:32 -0700

    fixed Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,17 +4,17 @@
  */
 void jack_bauer(void)
 {
-	int hour, minutes;
+	int hour, minute;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minutes = 0; minutes <= 59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((minutes / 10) + '0');
-			_putchar((minutes % 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
