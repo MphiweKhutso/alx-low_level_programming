6985cc5bd7a10ffa75153599633ef607d47b54d5
Khutso Mphiwe <mphiwekhutso@gmail.com>
2022-10-26 18:53:18 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings values
+ * @s1: string 1
+ * @s2: string 2
+ * Return: the differance in s1 abd s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
+}
