58f5ef11649dff01a0df4457781d2964b8523885
Khutso Mphiwe <mphiwekhutso@gmail.com>
2022-11-14 14:54:31 -0800

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,8 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> 
+
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
+
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
 #endif
